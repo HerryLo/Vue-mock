e202f413357be75eec60a287061ed6f002315a28
liuheng@9188.com <liuheng@9188.com>
2017-10-11 19:35:10 +0800

    'commit'

---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+    <header class='TalionNav'>
+    <div id="header">
+            <a href="/"><h1>豆瓣</h1></a>
+            <nav>
+                <ul>
+                <li>
+                    <a href="/movie" style="color: #2384E8;">{{name[0]}}</a>
+                </li>
+                <li>
+                    <a href="/book" style="color: #9F7860;">{{name[1]}}</a>
+                </li>
+                <li>
+                    <a href="/status" style="color: #E4A813;">{{name[2]}}</a>
+                </li>
+                <li>
+                    <a href="/group" style="color: #2AB8CC;">{{name[3]}}</a>
+                </li>
+                </ul>
+                <span class="aaa"></span>
+            </nav>
+    </div>
+    </header>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      name: ['电影', '图书', '广播', '小组']
+    }
+  }
+}
+</script>
+<style>
+#header{
+    border-bottom: 1px solid #f3f3f3;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 0 18px;
+    background: #fff;
+    height: 47px;
+    box-sizing: border-box;
+    display: -webkit-box;
+    display: flex;
+}
+a {
+    color: #42bd56;
+    text-decoration: none;
+}
+#header>* {
+    white-space: nowrap;
+}
+.TalionNav {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9999;
+    background: #fff;
+}
+#header nav>* {
+    vertical-align: middle;
+}
+.TalionNav a {
+    text-decoration: none;
+}
+#header>* {
+    white-space: nowrap;
+}
+#header nav>* {
+    /* vertical-align: middle; */
+}
+#header nav>*, #header nav li {
+    display: inline-block;
+}
+#header nav li {
+    font-size: 15px;
+    margin-right: 19px;
+}
+</style>
---
+++ src/components/quickNav.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul class="quick-nav">
+    <li>
+        <a href="/movie/nowintheater?loc_id=108288">影院热映</a>
+    </li>
+        <li>
+            <a href="/mine/">我的</a>
+        </li>
+    <li>
+        <a id="hot-topics" href="https://m.douban.com/time/?dt_time_source=douban-msite_shortcut">豆瓣时间</a>
+    </li>
+    <li>
+        <a href="https://www.douban.com/doubanapp/app?channel=card_home&amp;direct_dl=1">使用豆瓣App</a>
+    </li>
+</ul>
+</template>
+
+<script>
+    export default {}
+</script>
+<style>
+.quick-nav {
+    overflow: hidden;
+    margin: 20px 0 0 0;
+    padding-top: 6px;
+}
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.quick-nav>li {
+    float: left;
+    width: 50%;
+    padding: 3px;
+    box-sizing: border-box;
+    font-size: 15px;
+}
+.quick-nav>li>a {
+    background-color: #f6f6f6;
+    color: #494949;
+    display: block;
+    text-align: center;
+    line-height: 20px;
+    padding: 12px 0;
+    border-radius: 2px;
+    max-width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    word-wrap: normal;
+}
+a {
+    color: #42bd56;
+    text-decoration: none;
+}
+</style>
+
